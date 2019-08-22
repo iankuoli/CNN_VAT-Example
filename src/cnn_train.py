4ab898ad3fe1a217e7a65cb2946d98654f0548d8
iankuoli <iankuoli@gmail.com>
2019-08-22 14:08:07 +0800

    Limit GPU memory growth

--- src/cnn_train.py
+++ src/cnn_train.py
@@ -9,6 +9,24 @@ import src.utils as utils
 import src.vat.vat2 as vat
 from src.losses.face_losses2 import arcface_loss, focal_loss_with_softmax
 
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Limiting GPU memory growth
+#
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    # Restrict TensorFlow to only use the first GPU
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Visible devices must be set before GPUs have been initialized
+        print(e)
+
+
 # MNIST dataset parameters. Total classes (0-9 digits).
 num_classes = 10
 
